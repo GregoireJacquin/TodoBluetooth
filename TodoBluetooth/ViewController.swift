dcf26557b47b63422a5d039d5003012b3ab0ea8d
Grégoire Jacquin <gregoire.jacquin@me.com>
2019-10-14 21:05:28 +0200

    Initial Commit

---
+++ TodoBluetooth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoBluetooth
+//
+//  Created by Grégoire Jacquin on 14/10/2019.
+//  Copyright © 2019 Grégoire Jacquin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodoBluetoothTests/TodoBluetoothTests.swift
@@ -0,0 +1,34 @@
+//
+//  TodoBluetoothTests.swift
+//  TodoBluetoothTests
+//
+//  Created by Grégoire Jacquin on 14/10/2019.
+//  Copyright © 2019 Grégoire Jacquin. All rights reserved.
+//
+
+import XCTest
+@testable import TodoBluetooth
+
+class TodoBluetoothTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
